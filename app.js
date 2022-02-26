8b144e13d74f8a4439e9786f5adc4d9d48de4af4
A PONTEW <giselisantos2019@gmail.com>
2022-02-25 21:10:24 -0300

    maior valor

---
+++ app.js
@@ -0,0 +1,12 @@
+function calculo(){
+  var semaforo=prompt("Digite a cor do semaforo")
+  
+  if(semaforo == "verde"){
+      alert("Pode passar");
+  } else if (semaforo == "vermelho"){
+    alert("pare");   
+  } else {
+    alert("atenção"); 
+  }
+  }
+  
